ffc097f154059835364f380ffda672df93220f20
meltham44 <75094502+meltham44@users.noreply.github.com>
2025-02-09 14:24:43 +0000

    Add spinning cube

--- Assets/NumberGenerator.cs
+++ Assets/NumberGenerator.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class NumberGenerator : MonoBehaviour, IInteractable // accepts calls via the IInteractable interface
 {
+    public Transform cube;
     public void Interact(){
         Debug.Log(Random.Range(0, 100)); // code to run when object is interacted with
+        cube.Rotate(Vector3.up);
     }
 }
